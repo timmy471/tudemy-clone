bf5ced1cd772b69314a5b9eff248459acbf1d512
timmy471 <ayantunjitimilehin@yahoo.com>
2020-04-06 18:19:24 +0100

    retry

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+     Retry
+    </div>
+  );
+}
+
+export default App;
